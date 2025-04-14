395c3610c782613da65d58a870910c7d17277a88
Souvikarjun Deb <souvikarjundeb@gmail.com>
2025-04-14 09:43:20 +0530

    I will try another authentication app.

--- app/api/auth/[...nextauth]/route.js
+++
@@ -1,6 +0,0 @@
-import { handlers } from "@/auth" // Referring to the auth.ts we just created
-import NextAuth from "next-auth"
-
-const handler = NextAuth()
-
-export { handler as GET, handler as POST }
